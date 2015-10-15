301b01cc86ad8d31bd61cc3a826c698aa183411c
usuario <usuario@10.0.2.15>
2015-10-15 10:50:06 +0200

    Finalizamos ejercicio 51 y 5.2

--- Unidad1AD/src/U1A5/U1A5Ej1.java
+++ Unidad1AD/src/U1A5/U1A5Ej1.java
@@ -5,13 +5,12 @@
  */
 package U1A5;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StreamTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -21,27 +20,47 @@ public class U1A5Ej1 {
 
     public static void main(String[] args) {
         FileReader r;
+        FileWriter salida;
+        File f;
+        int numLineas;
         try {
-            FileWriter salida = new FileWriter("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\Salida.txt");
-        } catch (IOException ex) {
-            System.err.println("No se pudo acceder al fichero");
-        }
-        for (String fichero : args) {
-            try {
-                r = new FileReader(fichero);
-                StreamTokenizer tokens = new StreamTokenizer(r);
-                while (tokens.nextToken() != StreamTokenizer.TT_EOF) {
-                    if (tokens.ttype == StreamTokenizer.TT_WORD) {
-                        System.out.println(tokens.sval);
-                    } else if (tokens.ttype == StreamTokenizer.TT_NUMBER) {
-                        System.out.println((int) tokens.nval);
+            salida = new FileWriter("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\Salida.txt", false);
+            for (String fichero : args) {
+                try {
+                    System.out.println(fichero);
+                    f = new File("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\" + fichero);
+                    if (f.getName().matches("\\w+.txt")) {
+                        r = new FileReader(f);
+                        StreamTokenizer tokens = new StreamTokenizer(r);
+                        //bucle que recorra cada linea y aumente el contador
+
+                        numLineas = 0;
+                        while (tokens.nextToken() != StreamTokenizer.TT_EOF) {
+                            //TODO NO ME LEE EL PUTO EOL, las palabras si pero EOL no, WHY?
+                            if (tokens.ttype == StreamTokenizer.TT_EOL) {
+                                numLineas++;
+                                System.out.println("NumLineas por iteracion " + numLineas);
+                            }
+                        }
+                        System.out.println("NumLineas totales " + numLineas);
+                        salida.write("Nombre Fichero: " + fichero + " Num. Lineas: " + numLineas + ", ");
+                        r.close();
+                    } else {
+                        System.out.println("No es un txt");
+                        salida.write("Nombre Fichero: " + fichero + " No es un text, ");
                     }
+
+                } catch (FileNotFoundException ex) {
+                    System.out.println("file not found");
+                    salida.write("Nombre Fichero: " + fichero + " Error, fichero no encontrado, ");
+                } catch (IOException ex) {
+                    System.out.println("Io exception");
+                    salida.write("Nombre Fichero: " + fichero + " Error, IO exception, ");
                 }
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(U1A5Ej1.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(U1A5Ej1.class.getName()).log(Level.SEVERE, null, ex);
             }
+            salida.close();
+        } catch (IOException ex) {
+            System.out.println("Io exception 2");
         }
     }
 }
---
+++ Unidad1AD/src/U1A5/U1A5Ej2.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package U1A5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+/**
+ *
+ * @author usuario
+ */
+public class U1A5Ej2 {
+
+    public static void main(String[] args) {
+        FileReader r, r2;
+        FileWriter salida;
+        File f, f2;
+        int numPalabras=0;
+        try {
+            salida = new FileWriter("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\ContadorPalabras.txt", false);
+            f = new File("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\palabrasClave.txt");
+            r = new FileReader(f);
+            f2 = new File("C:\\Users\\usuario\\Desktop\\ADAT\\Ejercicios\\palabras.txt");
+            r2 = new FileReader(f2);
+            StreamTokenizer tokens = new StreamTokenizer(r);
+            StreamTokenizer tokens2 = new StreamTokenizer(r2);
+            //bucle que recorra cada linea y aumente el contador            
+            while (tokens.nextToken() != StreamTokenizer.TT_EOF) {
+                if (tokens.ttype == StreamTokenizer.TT_WORD) {
+                    tokens2 = new StreamTokenizer(new FileReader(f2));
+                    numPalabras=0;
+                    while (tokens2.nextToken() != StreamTokenizer.TT_EOF) {
+                        if (tokens.ttype == StreamTokenizer.TT_WORD) {
+                            //si el token de la lista de palbras es igual
+                            //al token del texto aumentamos contador
+                            if(tokens.sval.equals(tokens2.sval)) {
+                                numPalabras++;
+                            }
+                        }
+                    }
+                    salida.write(tokens.sval+" : "+numPalabras+", ");
+                }
+            }
+            r.close();
+            r2.close();
+            salida.close();
+        } catch (IOException ex) {
+            System.out.println("IOException 2");
+        }
+    }
+
+}
